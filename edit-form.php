03e061088298ed3d602753196931c271d7ea363e
Эрик <ErnesT@MacBook-Pro-Erik.local>
2019-03-24 12:44:06 +0100

    edit

--- edit-form.php
+++ edit-form.php
@@ -1,4 +1,19 @@
+<?php
+session_start();
+if(!isset($_SESSION['user_id'])) {
+    header('Location: /untitled/github/task-maneger/login-form.php');
+    exit;
+}
 
+$id = $_GET['id'];
+
+//подготовка и выполнение запроса к БД
+$pdo = new PDO("mysql:host=localhost; dbname=maneger", 'root', 'mysql');
+$sql = 'SELECT * from task where id=:id';
+$statement = $pdo->prepare($sql);
+$statement->execute([':id'  =>  $id]);
+$task = $statement->fetch(PDO::FETCH_ASSOC);
+?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -17,15 +32,15 @@
 
   <body>
     <div class="form-wrapper text-center">
-      <form class="form-signin">
+      <form class="form-signin" action="edit_update.php?id=<?= $task['id']; ?>" method="post" enctype="multipart/form-data">
         <img class="mb-4" src="assets/img/bootstrap-solid.svg" alt="" width="72" height="72">
         <h1 class="h3 mb-3 font-weight-normal">Добавить запись</h1>
         <label for="inputEmail" class="sr-only">Название</label>
-        <input type="text" id="inputEmail" class="form-control" placeholder="Название" required value="Выполнить все задания и сдать работу преподавателю">
+        <input type="text" name="title" id="inputEmail" class="form-control" placeholder="Название" required value="<?= $task['title']; ?>">
         <label for="inputEmail" class="sr-only">Описание</label>
-        <textarea name="description" class="form-control" cols="30" rows="10" placeholder="Описание">Пройти первый а потом второй урок. Закрепить практикой и написать проект сначала без подглядываний.</textarea>
-        <input type="file">
-        <img src="assets/img/no-image.jpg" alt="" width="300" class="mb-3">
+        <textarea name="description" class="form-control" cols="30" rows="10" placeholder="Описание"><?= $task['description']; ?></textarea>
+        <input type="file" name="img">
+        <img src="assets/img/<?= $task['img']; ?>" alt="" width="300" class="mb-3">
         <button class="btn btn-lg btn-success btn-block" type="submit">Редактировать</button>
         <p class="mt-5 mb-3 text-muted">&copy; 2018-2019</p>
       </form>
